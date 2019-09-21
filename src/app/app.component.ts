3ede54d873c2d855649455485bacb03378a17bf6
bhagesh.kalekar <bhagesh.kalekar@gmail.com>
2019-09-21 19:10:23 +0530

    core.module branch created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-perf';
+  constructor(
+    private http:HttpClient
+  ) {
+
+  }
+  ngOnInit() {
+    this.http.get('api/posts').subscribe();
+  }
 }
